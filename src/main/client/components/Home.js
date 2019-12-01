7ef320276ffa3373a2e5894d7128335a6302fd12
Jere Pallaskorpi <jere.pallaskorpi@gmail.com>
2019-12-01 19:01:30 +0200

    Improve styles

--- src/main/client/components/Home.js
+++ src/main/client/components/Home.js
@@ -5,32 +5,57 @@ import {
     ScrollView,
     Text,
 } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
 import {
-    Container, Input, InputContainer, Title, FooterButtons,
+    Container,
+    Input,
+    InputContainer,
+    Title,
+    FooterButtons,
+    MessageContainer,
+    TopContainer,
+    DeleteContainer,
 } from './styles';
+import config from '../config';
 
 function Home() {
-    const [title] = useState('React Native Starter Kit');
+    const [title] = useState('');
     const [messages, setMessages] = useState([]);
     const [inputText, setInputText] = useState('');
 
-    const handleSubmitInputText = async (text: string) => {
+    const handleSubmitInputText = async (message: String) => {
         setInputText('');
         Keyboard.dismiss();
 
-        const messageResponse: Object = await fetch(`http://localhost:8080/api/message`, {
+        fetch(`${config.api.host}/api/message`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
             method: 'POST',
-            body: text,
+            body: message
         })
             .then((r) => r.json())
+            .then((r) => setMessages(r))
             .catch((err) => console.log(err));
-        
-        setMessages(messageResponse);
+    };
+
+    const handleDeleteMessage = async (message: Object) => {
+        fetch(`${config.api.host}/api/message`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'DELETE',
+            body: JSON.stringify(message),
+        })
+            .then((r) => r.json())
+            .then((r) => setMessages(r))
+            .catch((err) => console.log(err));
     };
     
     const getMessages = async () => {
-        const messageResponse: Object[] = await fetch(`http://localhost:8080/api/message`)
+        const messageResponse: Object[] = await fetch(`${config.api.host}/api/message`)
             .then((r) => r.json())
             .catch((err) => console.log(err));
 
@@ -43,14 +68,23 @@ function Home() {
 
     return (
         <Container behavior="padding" enabled>
+            <TopContainer>
+                <Title color="orange">{title}</Title>
+            </TopContainer>
             <ScrollView>
-                <Title color="yellow">{title}</Title>
                 {messages.map((message) => (
-                    <Title key={message.id}>
+                    <MessageContainer key={message.id}>
                         <Text>{message.id}</Text>
-                        <Text> - </Text>
                         <Text>{message.message}</Text>
-                    </Title>
+                        <DeleteContainer>
+                            <FontAwesome
+                                onPress={() => handleDeleteMessage(message)}
+                                name="remove"
+                                size={36}
+                                color="white"
+                            />
+                        </DeleteContainer>
+                    </MessageContainer>
                 ))}
             </ScrollView>
             <FooterButtons>
@@ -60,10 +94,15 @@ function Home() {
                         value={inputText}
                     />
                     <AntDesign
-                        onPress={() => handleSubmitInputText(inputText)}
+                        onPress={() => inputText.trim().length > 0 
+                            && handleSubmitInputText(inputText)
+                        }
                         name="pluscircle"
                         size={48}
-                        color="yellow"
+                        color={inputText.trim().length > 0 
+                            ? "lightseagreen" 
+                            : "gray"
+                        }
                     />
                 </InputContainer>
             </FooterButtons>
--- src/main/client/components/styles.js
+++ src/main/client/components/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Constants from 'expo-constants';
 
 export const Title = styled.Text`
-    font-size: 24px;
+    font-size: 36px;
     padding: 8px;
     text-align: center;
     color: ${(props) => (props.color ? props.color : 'white')};
@@ -13,6 +13,27 @@ export const InputContainer = styled.View`
     justify-content: space-between;
     margin: 0 8px;
     flex: 1;
+    color: lightgreen;
+`;
+
+export const TopContainer = styled.View`
+    background: #fafafa;
+    box-shadow: 0 2px 4px hsla(0, 0%, 0%, 0.4);
+    padding-top: ${ Constants.statusBarHeight };
+    elevation: 2;
+`;
+
+export const MessageContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    flex: 1;
+    align-items: center;
+    background: #fafafa; 
+    margin: 5px;
+    border-radius: 5px;
+    box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.3);
+    padding-left: 10px;
+    elevation: 1;
 `;
 
 export const Input = styled.TextInput`
@@ -22,6 +43,7 @@ export const Input = styled.TextInput`
     flex: 1;
     padding: 8px;
     margin-right: 8px;
+    border: 1px solid #dfdfdf;
 `;
 
 export const Container = styled.KeyboardAvoidingView`
@@ -29,15 +51,20 @@ export const Container = styled.KeyboardAvoidingView`
     justify-content: space-between;
     flex-direction: column;
     flex: 1;
-    text-align: center;
-    align-items: center;
-    background: palevioletred;
-    padding-top: ${Constants.statusBarHeight};
+    background: #ffffff;
 `;
 
 export const FooterButtons = styled.View`
-    width: 100%;
     flex-direction: row;
     justify-content: space-around;
-    margin: 16px;
+    padding: 16px;
+    background: #fafafa;
+    box-shadow: 0 2px 4px hsla(0, 0%, 0%, 0.4);
+    elevation: 5;
+`;
+
+export const DeleteContainer = styled.View`
+    background: #FF4A54;
+    border-radius: 5px;
+    padding: 5px 10px;
 `;
